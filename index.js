820894bb3fd1305105bef9476e9a19a8b37ee677
25garcial <25garcial@camraiders.com>
2022-04-07 09:48:06 -0500

    .

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ this.author=author;
 this.pages=pages;
 this.beenRead=beenRead;
 this.rating=rating
+return {author, pages, beenRead, rating}
 }
 function newBook(){
-    book(
-        prompt("What is the name of the book?"),
-        prompt("have you read the book?"),
-        prompt("What is the author of the book?"),
-        prompt("how many pages are in the book?"),
-        prompt("rating from 1 to 5:"),
-        
-        )
+   
+    
+        var createNewBook=new book(
+            prompt("what is the name of the book?"),
+            prompt("have you read the book?"),
+            prompt("What is the author of the book?"),
+            prompt("How many pages are in the book?"),
+            prompt("Rating from 1 to 5:"), 
+            )
+        var library= document.getElementsByClassName("library")[0]
+        alert("1")
+        var newBook= document.createElement("div")
+        newBook.classList.add("book")
+        alert("2")
+         newBook.textContent=`Name=${createNewBook.name} Author=${createNewBook.author} Pages=${createNewBook.pages} Read=${createNewBook.read} Rating=${createNewBook.rating}`;
+        alert("3")
+        library.appendChild(newBook)
+        alert("4")
 }
