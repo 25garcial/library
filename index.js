41a9d7d6201bc154a9488a7bdbf8014ae99a4b4d
25garcial <25garcial@camraiders.com>
2022-04-12 09:29:30 -0500

    .

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 function book(name,author, pages,beenRead){
-this.name=name;
-this.author=author;
-this.pages=pages;
-this.beenRead=beenRead;
-}
-function newBook(){
-document.querySelector(".popUp").classList.add("On")
-
-    var newBook=new book()
-    var newCard=document.createElement("div")
-    newCard.classList.add("card")
-}
-function addCard(){
-    //adding the name
-    newCard.appendChild(document.createElement("a"))
-    newCard.lastElementChild.textContent=`Title=${newBook.name}`
-    newCard.lastElementChild.classList.add("name")
-    newCard.append(document.createElement("br"))
-    
-    //adding the author
-    newCard.appendChild(document.createElement("a"))
-    newCard.lastElementChild.textContent=`Author= ${newBook.author}`
-    newCard.lastElementChild.classList.add("author")
-    newCard.append(document.createElement("br"))
-    
-    //adding the pages
-    newCard.appendChild(document.createElement("a"))
-    newCard.lastElementChild.textContent=`number of pages= ${newBook.pages}`
-    newCard.lastElementChild.classList.add("pages")
-    newCard.append(document.createElement("br"))
-    
-    //adding the been read
-    newCard.appendChild(document.createElement("a"))
-    newCard.lastElementChild.textContent=`have you read this book?= ${newBook.beenRead}`
-    newCard.lastElementChild.classList.add("read")
-    newCard.append(document.createElement("br"))
-    document.querySelector(".library").appendChild(newCard)
+    this.name=name;
+    this.author=author;
+    this.pages=pages;
+    this.beenRead=beenRead;
     }
+    function newBook(){
+    
+    
+    }
+    function addCard(newCard, title, author, pages, read){
+        //toggle class
+        document.querySelector(".popUp").classList.add("On")
+        
+        //create card
+        var newCard=document.createElement("div")
+        newCard.classList.add("card") 
+    
+        //create book
+        var newBook=new book(title, author, pages, read)
+        document.querySelector(".popUp").classList.add("On") 
+    
+        //adding the name
+        newCard.appendChild(document.createElement("a"))
+        newCard.lastElementChild.textContent=`Title=${newBook.name}`
+        newCard.lastElementChild.classList.add("name")
+        newCard.append(document.createElement("br"))
+        
+        //adding the author
+        newCard.appendChild(document.createElement("a"))
+        newCard.lastElementChild.textContent=`Author= ${newBook.author}`
+        newCard.lastElementChild.classList.add("author")
+        newCard.append(document.createElement("br"))
+        
+        //adding the pages
+        newCard.appendChild(document.createElement("a"))
+        newCard.lastElementChild.textContent=`number of pages= ${newBook.pages}`
+        newCard.lastElementChild.classList.add("pages")
+        newCard.append(document.createElement("br"))
+        
+        //adding the been read
+        newCard.appendChild(document.createElement("a"))
+        newCard.lastElementChild.textContent=`have you read this book?= ${newBook.beenRead}`
+        newCard.lastElementChild.classList.add("read")
+        newCard.append(document.createElement("br"))
+        document.querySelector(".library").appendChild(newCard)
+        }
+    
+    document.querySelector(".submit").addEventListener("click", function(){
+        addCard( document.getElementsByName("title")[0].value, document.getElementsByName("author")[0].value, document.getElementsByName("pages")[0].value, document.getElementsByName("read")[0].value)
+    })
